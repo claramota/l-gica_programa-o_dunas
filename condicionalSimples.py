e8b82d061c85f46b46d7eb97252f3fac04747be0
MARIA CLARA MOTA COELHO <202403763851@alunos.unifanor.edu.br>
2025-03-28 10:04:38 -0300

    operadores aritméticos

--- condicionalSimples.py
+++ condicionalSimples.py
@@ -1,3 +1,5 @@
 numero=10
 if numero ==10:
     print("o numero informado é igual a 10")
+    print("o numeo informado e igual a 20")
+    print("o numero informado e igual a 30")
---
+++ operadoresAritmeticos.py
@@ -0,0 +1,29 @@
+#adição
+numero1= 10
+numero2= 20
+soma = numero1 + numero2 
+print(soma)
+
+#subtração 
+subtracao = numero1 - numero2
+print(subtracao)
+
+#multiplcação 
+multiplicacao = numero1 * numero2
+print(multiplicacao)
+
+#divisão
+divisao = numero1 / numero2
+print(divisao)
+
+#potência
+potencia = numero1 ** numero2
+print(potencia)
+
+#modulo
+modulo = numero1 % numero2
+print(modulo)
+
+#resto da divisão 
+restodadivisao = numero1 // numero2
+print(restodadivisao)
