2b5c6b61fb10f4d479a7373f2a4c8af6b3402488
MARIA CLARA MOTA COELHO <202403763851@alunos.unifanor.edu.br>
2025-03-28 10:54:02 -0300

    operadores de comparação

---
+++ operadoresComparacao.py
@@ -0,0 +1,20 @@
+x = 10
+y = 10
+
+#comparação de igualdade:
+print(x == y)
+
+#diferença 
+print(x != y)
+
+#superioridade 
+print(x > y)
+
+#inferioridade
+print(x < y)
+
+#maior ou igual
+print(x >= y)
+
+#menor ou igual 
+print (x <= y)
