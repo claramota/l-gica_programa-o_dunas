ba501a1ec4937f40bab958fab89ee01debb0bb25
MARIA CLARA MOTA COELHO <202403763851@alunos.unifanor.edu.br>
2025-04-04 08:55:38 -0300

    Desafio 001

---
+++ desafio001.py
@@ -0,0 +1,14 @@
+x=10
+y=10
+if x==y:
+    print("Os valores são iguais.")
+
+x=5
+y=10
+if x==y:
+    print("Os valores sao iguais.")
+
+x1=5
+y1=6
+if x1==y1:
+    print("os valores sao iguais.")
---
+++ desafio01.py
@@ -0,0 +1,4 @@
+x=2
+y=4
+if x==y:
+    print("os valores sao iguais.")
